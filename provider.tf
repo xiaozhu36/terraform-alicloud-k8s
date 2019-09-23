1c054f128bddf0c8c65f80689644d92a051adfa4
He Guimin <guimin.hgm@alibaba-inc.com>
2019-09-23 10:27:38 +0800

    add a new field configuration_source to tag this module

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 provider "alicloud" {
-  version    = ">=1.53.0"
-  access_key = var.ali_access_key
-  secret_key = var.ali_secret_key
-  region     = var.ali_region
+  version              = ">=1.56.0"
+  access_key           = var.ali_access_key
+  secret_key           = var.ali_secret_key
+  region               = var.ali_region
+  configuration_source = "hajowieland/k8s"
 }
